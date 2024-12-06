ababb03ae1b0a7b4e8039523449542565b0b705f
Chetan Manker <148444931+Chetan2520@users.noreply.github.com>
2024-12-06 10:34:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import WeightGain from './WeightGain';
 import { Outlet } from 'react-router-dom'; // Use Outlet for nested routes
 import Home from './Home';
 import Footer from './Footer';
-import ScrollToTop from './ScrolltoTop';
+
 
 function App() {
   return (
     <>
       <Navbar />
-      <ScrollToTop /> 
+
       <Outlet /> {/* Handles routing via nested routes */}
       <Footer />
     </>
